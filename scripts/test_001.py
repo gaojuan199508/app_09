d09378403d0f56a1c31cf4cb9d2510c01ef4f699
unknown <1054867170@qq.com>
2019-05-27 10:45:52 +0800

    添加

---
+++ scripts/test_001.py
@@ -0,0 +1,15 @@
+import pytest
+
+import allure
+
+
+class Test001:
+
+    @pytest.allure.severity(pytest.allure.severity_level.BLOCKER)
+    @allure.step(title="这是一个测试步骤01")
+    def test_001_1(self):
+        print("test_001_1")
+        allure.attach("标题", "具体内容")
+        assert True
+
+
